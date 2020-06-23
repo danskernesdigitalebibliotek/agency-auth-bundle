d417ae95effc54f987331dd917252de2a7f0ced7
Ture Gjørup <ture@ignatz.dk>
2020-06-23 12:43:39 +0200

    DDBTEAM-538: Split testing kernel into separate file

--- tests/BundleTest.php
+++ tests/BundleTest.php
@@ -6,20 +6,17 @@
 
 namespace DanskernesDigitaleBibliotek\AgencyAuthBundle\Tests;
 
-use DanskernesDigitaleBibliotek\AgencyAuthBundle\DanskernesDigitaleBibliotekAgencyAuthBundle;
 use DanskernesDigitaleBibliotek\AgencyAuthBundle\Security\TokenAuthenticator;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpClient\CurlHttpClient;
-use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * Class BundleTest.
  */
 class BundleTest extends TestCase
 {
+    /**
+     * Test service wirring
+     */
     public function testServiceWiring()
     {
         $kernel = new DanskernesDigitaleBibliotekAgencyAuthBundleTestingKernel('test', true);
@@ -29,27 +26,5 @@ class BundleTest extends TestCase
         $tokenAuthenticator = $container->get('ddb.agency_token_auth');
         $this->assertInstanceOf(TokenAuthenticator::class, $tokenAuthenticator);
         $this->assertFalse($tokenAuthenticator->supportsRememberMe());
-    }
-}
-
-/**
- * Class DanskernesDigitaleBibliotekAgencyAuthBundleTestingKernel.
- */
-class DanskernesDigitaleBibliotekAgencyAuthBundleTestingKernel extends Kernel
-{
-    /** {@inheritdoc} */
-    public function registerBundles()
-    {
-        return [
-            new DanskernesDigitaleBibliotekAgencyAuthBundle(),
-        ];
-    }
-
-    /** {@inheritdoc} */
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(function (ContainerBuilder $containerBuilder) {
-            $containerBuilder->register(HttpClientInterface::class, CurlHttpClient::class);
-        });
     }
 }
---
+++ tests/DanskernesDigitaleBibliotekAgencyAuthBundleTestingKernel.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file
+ * Minimal kernel for testing
+ */
+
+namespace DanskernesDigitaleBibliotek\AgencyAuthBundle\Tests;
+
+use DanskernesDigitaleBibliotek\AgencyAuthBundle\DanskernesDigitaleBibliotekAgencyAuthBundle;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * Class DanskernesDigitaleBibliotekAgencyAuthBundleTestingKernel.
+ */
+class DanskernesDigitaleBibliotekAgencyAuthBundleTestingKernel extends Kernel
+{
+    /** {@inheritdoc} */
+    public function registerBundles()
+    {
+        return [
+            new DanskernesDigitaleBibliotekAgencyAuthBundle(),
+        ];
+    }
+
+    /** {@inheritdoc} */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(function (ContainerBuilder $containerBuilder) {
+            $containerBuilder->register(HttpClientInterface::class, CurlHttpClient::class);
+        });
+    }
+}
